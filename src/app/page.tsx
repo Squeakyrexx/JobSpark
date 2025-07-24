8650b1b4a57f8da33104a9cb879e855e6819e40b
rowan johnson <rowanojohnson9@gmail.com>
2025-07-24 02:13:42 +0000

    it failed to deploy [22:10:33.016] Running build in Washington, D.C., US

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { SearchForm } from '@/components/search-form';
+import { Suspense } from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
+
+function SearchFormSkeleton() {
+  return <Skeleton className="w-full h-[550px] rounded-2xl" />;
+}
 
 export default function Home() {
   return (
     <div className="fade-in">
-      <SearchForm />
+       <Suspense fallback={<SearchFormSkeleton />}>
+        <SearchForm />
+      </Suspense>
     </div>
   );
 }
