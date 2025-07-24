f69e35f758cb2c97c0fdcc08532437b458326316
rowan johnson <rowanojohnson9@gmail.com>
2025-07-24 01:25:44 +0000

    can you make the search section fit better with the screen right now it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { SearchForm } from '@/components/search-form';
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-start pt-12 md:pt-20 fade-in">
-      <div className="w-full max-w-2xl">
-        <SearchForm />
-      </div>
+    <div className="fade-in">
+      <SearchForm />
     </div>
   );
 }
